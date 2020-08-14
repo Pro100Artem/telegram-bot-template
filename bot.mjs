ba881994547b60b3bb93a24a25eeb4e007d16f67
ExposedCat <44642024+ExposedCat@users.noreply.github.com>
2020-08-14 16:17:25 +0300

    Remove functions definition from main()
    Remove handlers and save function definition from main function

--- bot.mjs
+++ bot.mjs
@@ -1,43 +1,43 @@
 import tools from 'satanic'
 
+const env = tools.readEnv()
+const hideErrors = env.HIDE_ERRORS === 'true'
+const admins = env.ADMINS.split(', ').map(id => +id)
+const bot = tools.bot(env.TOKEN, hideErrors)
+const client = bot.telegram
+
 async function main() {
     tools.updateClasses(/* Prototypes to update */)
-
-    const env = tools.readEnv()
-    const hideErrors = env.HIDE_ERRORS === 'true'
-    const bot = tools.bot(env.TOKEN, hideErrors, () => {
-        tools.doAtDate(env.SAVING_DATA_CRON, async () => {
-            await saveAll()
-        })
+    tools.doAtDate(env.SAVING_DATA_CRON, async () => {
+        await saveAll()
     })
-    const admins = env.ADMINS.split(', ').map(id => +id)
-
-    async function saveAll() {
-
-    }
-
-    async function handleText(ctx) {
-        const text = ctx.message.text
-        const userId = ctx.from.id
-        const chatId = ctx.chat.id
-
-        if (admins.includes(userId)) {
-
-        } else {
-
-        }
-    }
-    async function handleCbQuery(ctx) {
-        const cb = ctx.callbackQuery
-        const data = cb.data
-        const userId = ctx.from.id
-
-
-    }
-
     bot.on('text', handleText)
     bot.on('callback_query', handleCbQuery)
-
     bot.start()
 }
+
+async function saveAll() {
+    
+}
+
+async function handleText(ctx) {
+    const text = ctx.message.text
+    const userId = ctx.from.id
+    const chatId = ctx.chat.id
+
+    if (admins.includes(userId)) {
+        // admin
+    } else {
+        // user
+    }
+}
+
+async function handleCbQuery(ctx) {
+    const cb = ctx.callbackQuery
+    const data = cb.data
+    const userId = ctx.from.id
+
+
+}
+
 main()
